179cbf05c12700b4ac65f260b0bc0e38fda3396e
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-07-03 09:38:16 -0400

    ADD: /bin/bash

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-cp ./JetBrains/JetBrainsMonoNerdFontMono-Regular.ttf $HOME/.local/share/fonts/ -v
-cp ./SpaceMono/SpaceMonoNerdFont-Regular.ttf $HOME/.local/share/fonts/ -v
+#! /bin/bash
+
+cp JetBrains/JetBrainsMonoNerdFontMono-Regular.ttf $HOME/.local/share/fonts/ -v
+cp SpaceMono/SpaceMonoNerdFont-Regular.ttf $HOME/.local/share/fonts/ -v
 
 fc-cache -fv
