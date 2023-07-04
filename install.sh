d640aebd6519f0c30ffd17b9b1c4440d361d7492
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-07-04 02:13:48 -0400

    update install.s

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir $HOME/.local/share/fonts -p
+
 cp JetBrains/JetBrainsMonoNerdFontMono-Regular.ttf $HOME/.local/share/fonts/ -v
 cp SpaceMono/SpaceMonoNerdFont-Regular.ttf $HOME/.local/share/fonts/ -v
 
